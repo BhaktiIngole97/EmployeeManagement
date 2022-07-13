973374ab745e7c79e0ce337bd7e17dddfc3ab665
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-07-13 21:01:24 +0530

    Add project files.

---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to EmployeeManagement App");
+int IS_FULL_TIME = 1;
+Random random = new Random();
+int empCheck = random.Next(0, 2);
+if (empCheck == IS_FULL_TIME)
+{
+    Console.WriteLine("Employee is present");
+}
+else
+{
+
+    Console.WriteLine("Employee is absent");
+
+}
+
+
+
