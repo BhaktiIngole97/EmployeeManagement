07ee2660c484fed953f24d44159b137479bf95cc
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-07-14 13:06:24 +0530

    UC-3-AddParttime

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Constants
+int IS_PART_TIME = 1;
 int IS_FULL_TIME = 1;
 int EMP_RATE_PER_HOUR = 20;
 //Variables
@@ -7,13 +8,17 @@ int empHrs = 0;
 int empWage = 0;
 Random random = new Random();
 //Computation
-int empCheck = random.Next(0, 2);
-if (empCheck == IS_FULL_TIME)
+int empCheck = random.Next(0, 3);
+if (empCheck == IS_PART_TIME)
+{
+    empHrs = 4;
+}
+else if (empCheck == IS_FULL_TIME)
 {
     empHrs = 8;
 }
 else
-{
+{ 
     empHrs = 0;
 }
 empWage = empHrs * EMP_RATE_PER_HOUR;
