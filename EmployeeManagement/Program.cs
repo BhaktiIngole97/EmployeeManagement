28249487d0527554b026447a9147014787d93f25
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-07-14 14:39:44 +0530

    UC-6-WorkingHours

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -4,11 +4,13 @@
  const int IS_FULL_TIME = 2;
  const int EMP_RATE_PER_HOUR = 20;
  const int NUM_OF_WORKING_DAYS = 2;
+ const int MAX_HRS_IN_MONTH = 10;
 //Variables
-int empHrs = 0, empWage = 0, totalEmpWage = 0;
+int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 //Computation
-for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
+    totalWorkingDays++;
     Random random = new Random();
     int empCheck = random.Next(0, 3);
     switch (empCheck)
@@ -23,10 +25,10 @@ for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             empHrs = 0;
             break;
     }
-    empWage = empHrs * EMP_RATE_PER_HOUR;
-    totalEmpWage += empWage;
-    Console.WriteLine("Emp Wage : " + empWage);
+    totalEmpHrs += empHrs;
+    Console.WriteLine("Days#:" + totalWorkingDays + " Emp Hrs :" + empHrs);
 }
+int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 Console.WriteLine("Total Emp Wage : " + totalEmpWage);
 
 
