ce8eadbe7a37763eaf1da532cba0925efcaa5ef9
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-07-15 21:13:41 +0530

    UC-10-EmpWageBuilderArray

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,9 +1,9 @@
 ﻿using EmployeeManagement;
-EmpWageBuilderobject dMart = new EmpWageBuilderobject("Dmart", 20, 2, 10);
-EmpWageBuilderobject reliance = new EmpWageBuilderobject("Reliance", 10, 4, 20);
-dMart.computeEmpWage();
-Console.WriteLine(dMart.toString());
-reliance.computeEmpWage();
-Console.WriteLine(reliance.toString());
+
+EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+empWageBuilder.addCompanyWage("Dmart", 20, 2, 10);
+empWageBuilder.addCompanyWage("Reliance", 10, 4, 20);
+empWageBuilder.computeEmpWage();
+
 
 
